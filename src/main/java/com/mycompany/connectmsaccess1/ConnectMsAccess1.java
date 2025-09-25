e602e56d98f6d65ed9b54b1f7d77d49d75bdca7b
Administrator <Administrator@WIN-HRB4ISKF4DT>
2025-09-25 12:35:13 +0800

    commit

---
+++ src/main/java/com/mycompany/connectmsaccess1/ConnectMsAccess1.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.connectmsaccess1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Administrator
+ */
+public class ConnectMsAccess1 {
+
+    public static Connection conn() throws SQLException {
+
+        try {
+            String url = "jdbc:ucanaccess://C://Users//Administrator//Documents//Lab";
+            Connection conn = DriverManager.getConnection(url);
+            return conn;
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+        return null;
+    }
+
+}
